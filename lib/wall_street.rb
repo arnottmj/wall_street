7961e870938fcfda3679d992076f5d7ba84a0a15
Michael Arnott <arnottmj@aol.com>
2015-06-09 16:14:28 +0100

    completed main code

--- lib/wall_street.rb
+++ lib/wall_street.rb
@@ -1,5 +1,15 @@
 def max_profit array
 
-  array.combination(2).map {|i| i + [i[1]-i[0]]}
+  method = array.combination(2).map {|i| i + [i[1]-i[0]]}.sort_by {|j| j[2]}.last
+
+  buy_day = array.index(method[0])
+  sell_day = array.index(method[1])
+  profit = array[2]
+
+  return ["buy_day: #{buy_day}", "sell_day: #{sell_day}" ]
 
 end
+
+stockprice = [3,10,2,4,11,5,15,5,9,8]
+
+puts max_profit(stockprice)

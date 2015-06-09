95caaff50f0b0a961648f82ee7323fb1a90b5fbf
Michael Arnott <arnottmj@aol.com>
2015-06-09 18:48:19 +0100

    finished refactoring

--- lib/wall_street.rb
+++ lib/wall_street.rb
@@ -1,40 +1,32 @@
 def max_profit stock_prices
 
-  prices_index_by_day = stock_prices.map.with_index do |price, index|
-    [index, price]
-  end.to_h
+  prices_indexed_by_day = index_prices_by_day(stock_prices)
 
-  # produces a hash where the key is the day number and the hash
-  # is the stock price on that day
-
-  day_combinations = generate_day_combinations_from prices_index_by_day
-
-  # produces an array with all possible buy/sell day combinations
+  day_combinations = generate_day_combinations_from prices_indexed_by_day
 
   possible_profits = Hash.new
 
   day_combinations.each do |day_combo|
-    possible_profits[prices_index_by_day[day_combo[1]]- prices_index_by_day[day_combo[0]]] = day_combo 
+    possible_profits[prices_indexed_by_day[day_combo[1]]- prices_indexed_by_day[day_combo[0]]] = day_combo 
   end
-
-  # produces a hash where each key is the profit and 
-  # the value is the buy/sell day combo producing it
 
   maximum_profit = possible_profits.keys.max
 
-  #find the max profit
-
   most_profitable_buy_sell_combo = possible_profits[maximum_profit]
 
-  # finds the buy/sell combo that creates the max profit
 
-  return ["buy_day: #{most_profitable_buy_sell_combo[0]}",
-          "sell_day: #{most_profitable_buy_sell_combo[1]}"]
-
+  ["buy_day: #{most_profitable_buy_sell_combo[0]}",
+  "sell_day: #{most_profitable_buy_sell_combo[1]}"]
 end
 
 def generate_day_combinations_from array
   array.keys.combination(2)
+end
+
+def index_prices_by_day stock_prices
+  stock_prices.map.with_index do |price, index|
+    [index, price]
+  end.to_h
 end
 
 
--- spec/wall_street_spec.rb
+++ spec/wall_street_spec.rb
@@ -2,7 +2,7 @@ require 'wall_street'
 
 describe 'max_profit' do
 
-  it 'return "[buy_day: 5, sell_day: 8]" when stockprice = [10,2,5,8,9,1,6,7,9,8]' do
+  it 'return the most profitable buy day and sell day combination' do
     expect(max_profit([10,2,5,8,9,1,6,7,9,8])).to eq ['buy_day: 5', 'sell_day: 8']
   end
 
